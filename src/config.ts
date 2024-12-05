7d8f6990e115c4e3fd8e303c341202695b791b20
nachog00 <chonageo@protonmail.com>
2024-12-05 01:13:41 -0300

    Config file con global variables

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const PRODUCT_NAME = 'Telalarms';
+export const PRODUCT_DESCRIPTION = 'Telalarms es un sistema de alarmas que te permite proteger tu comunidad en cualquier momento con un simple llamado';
+export const PRODUCT_IMAGE = '/Logo.svg';
